7ce156dd1d915660b85208d017a2c3ac30e3e10b
drahdiwaberl <markus@photing.com>
2026-01-13 21:57:18 +0100

    ..

---
+++ public-dashboard.blade.php
@@ -0,0 +1,7 @@
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+    @foreach ($this->getHeaderWidgets() as $widget)
+        <div class="col-span-1">
+            @livewire($widget)
+        </div>
+    @endforeach
+</div>
--- resources/views/layouts/public-dashboard.blade.php
+++ resources/views/layouts/public-dashboard.blade.php
@@ -1,7 +1,3 @@
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-    @foreach ($this->getHeaderWidgets() as $widget)
-        <div class="col-span-1">
-            @livewire($widget)
-        </div>
-    @endforeach
+<div>
+    {{-- This file is not currently used by the PublicDashboard component. --}}
 </div>
