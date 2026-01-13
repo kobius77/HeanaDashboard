82853d4b818976d1787175fb71f66a12a51956c0
markusk <markus@photing.com>
2026-01-13 23:14:37 +0100

    by gemini-cli

--- resources/views/livewire/public-dashboard.blade.php
+++ resources/views/livewire/public-dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+<div class="grid grid-cols-1 gap-4">
     @foreach ($this->getHeaderWidgets() as $widget)
         <div class="col-span-1">
             @livewire($widget)
