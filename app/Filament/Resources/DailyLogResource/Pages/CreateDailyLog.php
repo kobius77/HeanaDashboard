a9eee13697b9a94a7ea60baba330fa5148f5ba54
markusk <markus@photing.com>
2026-01-30 00:08:45 +0100

    fix: add cross-database compatibility and improve dashboard for production

---
+++ app/Filament/Resources/DailyLogResource/Pages/CreateDailyLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DailyLogResource\Pages;
+
+use App\Filament\Resources\DailyLogResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDailyLog extends CreateRecord
+{
+    protected static string $resource = DailyLogResource::class;
+}
