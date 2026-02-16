354258961a8a6adbfe61ce1dcb43775708029280
markusk <markus@photing.com>
2026-02-16 13:50:17 +0100

    Add webhook/financial to CSRF exemption list

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'webhook/ingest',
+            'webhook/financial',
         ]);
         $middleware->web(append: [
             \App\Http\Middleware\SetUserLocale::class,
